ee4792eef79c96465b72d1acb243fb3c0f62897d
mohammad ali ebrahimzadeh <mohammad77.me@gmail.com>
2025-12-14 20:03:11 +0330

    added script

---
+++ Rira.Persistence/Scripts/InitDb.sql
@@ -0,0 +1,28 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Users] (
+    [Id] bigint NOT NULL IDENTITY,
+    [FirstName] nvarchar(100) NULL,
+    [LastName] nvarchar(100) NULL,
+    [NationalCode] nvarchar(10) NULL,
+    [BirthDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+
+CREATE UNIQUE INDEX [IX_Users_NationalCode] ON [Users] ([NationalCode]) WHERE [NationalCode] IS NOT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20251214092024_InitDb', N'9.0.0');
+
+COMMIT;
+GO
+
